f91076aed13e301458c30dec5062d71a6324ff69
Nazarov Ruslan <rnazarov142@gmail.com>
2023-11-11 15:05:44 +0200

    Добавьте файлы проекта.

---
+++ lab13_nazarov/Program.cs
@@ -0,0 +1,20 @@
+﻿//Высокий уровень, 23
+string[] students = { "Назаров Руслан", "Садик Назар", "Печкин Анатолий", "Печкин Андрей", "Фомичев Алексей",  };
+
+int[] studentsCount = new int[students.Length];
+
+for (int i = 0; i < students.Length; i++)
+{
+    for (int j = 0; j < students.Length; j++)
+    {
+        if (i != j && students[j].IndexOf(students[i].Split(' ')[0]) != -1)
+        {
+            studentsCount[i]++;
+        }
+    }
+}
+
+for (int i = 0; i < students.Length; i++)
+{
+    Console.WriteLine($"Студент {students[i]} имеет {studentsCount[i]} однофамильцев");
+}
