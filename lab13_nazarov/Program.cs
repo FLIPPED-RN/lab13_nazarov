5adb786da8340f38e149ffbf87713a4488960299
Nazarov Ruslan <rnazarov142@gmail.com>
2023-12-03 13:15:15 +0200

    lab13_nazarov

--- lab13_nazarov/Program.cs
+++ lab13_nazarov/Program.cs
@@ -1,20 +1,45 @@
-﻿//Высокий уровень, 23
-string[] students = { "Назаров Руслан", "Садик Назар", "Печкин Анатолий", "Печкин Андрей", "Фомичев Алексей",  };
+﻿//Высокий уровень, 23 (Назаров Руслан)
 
-int[] studentsCount = new int[students.Length];
-
-for (int i = 0; i < students.Length; i++)
+try
 {
-    for (int j = 0; j < students.Length; j++)
+    Console.Write("Введите строку с фамилиями и инициалами студентов: ");
+    string input = Console.ReadLine().ToUpper(); // Приводим все буквы к верхнему регистру для унификации
+    char[] mas = input.ToCharArray();
+
+    int[] countArray = new int[33];
+
+    foreach (char symbol in mas)
     {
-        if (i != j && students[j].IndexOf(students[i].Split(' ')[0]) != -1)
+        if (char.IsLetter(symbol))
         {
-            studentsCount[i]++;
+            int index = symbol - 'А';
+
+            if (index >= 0 && index < 33)
+            {
+                countArray[index]++;
+            }
+        }
+    }
+
+    Console.WriteLine("Группа с указанием однофамильцев:");
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (char.IsLetter(mas[i]))
+        {
+            int index = mas[i] - 'А';
+
+            if (index >= 0 && index < 33)
+            {
+                Console.Write($"{mas[i]} ({countArray[index]}) ");
+            }
+        }
+        else
+        {
+            Console.Write(mas[i]);
         }
     }
 }
-
-for (int i = 0; i < students.Length; i++)
+catch (Exception ex)
 {
-    Console.WriteLine($"Студент {students[i]} имеет {studentsCount[i]} однофамильцев");
+    Console.WriteLine(ex.Message);
 }
